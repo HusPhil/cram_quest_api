2ab198123694bba2407ecf65899aafc2fff7fb83
Fhil Joshua <husphil200@gmail.com>
2025-03-28 07:13:52 +0800

    switching to neon postgress db

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "sqlite:///./cram_quest.db"  # Change to PostgreSQL if needed
+    # DATABASE_URL: str = "sqlite:///./cram_quest.db"  # Change to PostgreSQL if needed
+    DATABASE_URL: str = f"postgresql://neondb_owner:{os.getenv('NEONDB_PASSWORD')}@ep-orange-mode-a13k4k1w-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"  # Change to PostgreSQL if needed
+
     SECRET_KEY: str = os.getenv("SECRET_KEY", "default_secret_key")  # Fallback in case .env is missing
     ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
